5e1b51eb055052d1d71af61c12fa07662ba98ae1
Safi <Safiullahkakar20202@outlook.com>
2023-12-07 16:32:25 +0430

    Day6 part 1 solution

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,8 +1,39 @@
 
+class ToyBoat(input: List<String>) {
+    private val speeds = mutableListOf<Int>()
+    private val records = mutableListOf<Int>()
+    private var errorMargin: Int = 1
+
+    init {
+        input.forEachIndexed { index: Int, s: String ->
+            val values = s.split(" ").mapNotNull { it.trim().toIntOrNull() }
+            if (index == 0) speeds.addAll(values) else records.addAll(values)
+        }
+    }
+
+    fun getMargin(): Int {
+        speeds.zip(records).forEach {
+            val time = it.first
+            val highest = it.second
+            var num = 0
+
+            for (i in 0..time) {
+                val currentHighest = (time - i) * i
+                if (currentHighest > highest) {
+                    num +=1
+                }
+            }
+            if (num != 0) {
+                errorMargin *= num
+            }
+        }
+        return errorMargin
+    }
+}
 
 fun main() {
     fun part1(input: List<String>): Int {
-        return - 1
+        return ToyBoat(input).getMargin()
     }
 
     fun part2(input: List<String>): Int {
@@ -11,10 +42,9 @@ fun main() {
 
     val input = readInput("Day06")
     val testInput = readInput("Day06_test")
-    check(part1(testInput) == -1)
-
+    check(part1(testInput) == 288)
     check(part2(testInput) == -1)
 
     part1(input).println()
-    part2(input).println()
+//    part2(input).println()
 }
