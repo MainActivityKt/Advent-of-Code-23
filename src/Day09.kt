bdb67020d16bb05c2c69f537f865f8fd7079626e
Safi <Safiullahkakar20202@outlook.com>
2023-12-10 20:55:46 +0430

    Day 9 part 2 solution

--- src/Day09.kt
+++ src/Day09.kt
@@ -16,43 +16,49 @@ fun findSequence(row:String): MutableList<MutableList<Long>> {
     return sequences
 }
 
-fun findNextValue(sequence: MutableList<MutableList<Long>>): Long {
-    sequence[sequence.lastIndex] += 0
-
-    for (i in sequence.lastIndex - 1 downTo  0) {
-        val nextValue = sequence[i].last() + sequence[i + 1].last()
-        sequence[i].add(nextValue)
+fun extrapolateNextValue(sequence: MutableList<MutableList<Long>>, extrapolateBackwards: Boolean = false): Long {
+    if (!extrapolateBackwards) {
+        sequence[sequence.lastIndex] += 0
+        for (i in sequence.lastIndex - 1 downTo  0) {
+            val nextValue = sequence[i].last() + sequence[i + 1].last()
+            sequence[i].add(nextValue)
+        }
+        return (sequence.first().last())
+    } else {
+        sequence.last().add(0, 0)
+        for (i in sequence.lastIndex - 1 downTo 0) {
+            val nextValue = sequence[i].first() - sequence[i + 1].first()
+            sequence[i].add(0, nextValue)
+        }
+        return sequence.first().first()
     }
 
-    return (sequence.first().last())
 }
 
-fun sumNextValues(input: List<String>): Long {
+fun getSumOfExtrapolatedValues(input: List<String>, getMoreHistory: Boolean = false): Long {
     var sum = 0L
     input.forEach { row ->
         val currentSequence = findSequence(row)
-        sum += findNextValue(currentSequence)
+        sum += extrapolateNextValue(currentSequence, getMoreHistory)
     }
-
-
     return sum
 }
 
 fun main() {
-    fun part1(input: List<String>): Int {
-
-        return -1
+    fun part1(input: List<String>): Long {
+        return getSumOfExtrapolatedValues(input)
     }
 
-    fun part2(input: List<String>): Int {
-        return -1
+    fun part2(input: List<String>): Long {
+        return getSumOfExtrapolatedValues(input, true)
     }
 
     val input = readInput("Day09")
     val testInput = readInput("Day09_test")
-//    check(part1(testInput) == -1)
-//    check(part2(testInput) == -1)
-//    part1(input).println()
-//    part2(input).println()
-    sumNextValues(input).println()
+
+    check(part1(testInput) == 114L)
+    check(part2(testInput) == 2L)
+    
+    part1(input).println()
+    part2(input).println()
 }
