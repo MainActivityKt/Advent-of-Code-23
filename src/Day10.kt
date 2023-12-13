72713b6865a1acbfbcab17cfef38ab3cf75d3e63
Safi <Safiullahkakar20202@outlook.com>
2023-12-13 18:46:11 +0430

    Day 10 part 1 solution cleaned and refactored

--- src/Day10.kt
+++ src/Day10.kt
@@ -1,28 +1,27 @@
 
 enum class Direction {
     NORTH, SOUTH, WEST, EAST
-
 }
 
 class Pipes(val input: List<String>) {
     private var animalRowIndex: Int = 0
-    var maxSteps = 0
-    var nextIndex = 0
+    private var animalIndex: Int = 0
+    private var maxSteps = 0
     init {
         input.forEachIndexed { index, s -> if (s.contains('S')) animalRowIndex = index  }
+        animalIndex = input[animalRowIndex].indexOf('S')
     }
 
     fun calculateSteps(): Int {
-        searchEast()
-        searchWest()
-        searchNorth()
-        searchSouth()
+        for (direction in Direction.entries) {
+            searchDirection(direction, animalIndex, animalRowIndex)
+        }
         return maxSteps
     }
 
-    fun searchEast() {
-        var direction = Direction.EAST
-        var currentIndex = input[animalRowIndex].indexOf('S') + 1
+    private fun searchDirection(startingDirection: Direction, animalIndex: Int, animalRowIndex: Int) {
+        var direction = startingDirection
+        var currentIndex = updateIndex(animalIndex, direction)
         var currentRow = animalRowIndex
         var currentPipe = input[currentRow][currentIndex]
         var steps = 0
@@ -36,80 +35,28 @@ class Pipes(val input: List<String>) {
         if ((steps + 1) / 2 > maxSteps)  maxSteps = (steps + 1) / 2
     }
 
-    fun searchWest() {
-        var direction = Direction.WEST
-        var currentIndex = input[animalRowIndex].indexOf('S') - 1
-        var currentRow = animalRowIndex
-        var currentPipe = input[currentRow][currentIndex]
-        var steps = 0
-        while (currentPipe != 'S') {
-            direction = updateDirection(currentPipe, direction)
-            currentRow = updateRow(currentRow, direction)
-            currentIndex = updateIndex(currentIndex, direction)
-            currentPipe = input[currentRow][currentIndex]
-            steps += 1
+    private fun updateDirection(pipe: Char, direction: Direction): Direction {
+        return when(pipe) {
+            'L' -> if (direction == Direction.WEST) Direction.NORTH else Direction.EAST
+            'J' -> if (direction == Direction.SOUTH) Direction.WEST else Direction.NORTH
+            '7' -> if (direction == Direction.EAST) Direction.SOUTH else Direction.WEST
+            'F' -> if (direction == Direction.WEST) Direction.SOUTH else Direction.EAST
+            else -> direction
         }
-        if ((steps + 1) / 2 > maxSteps)  maxSteps = (steps + 1) / 2
     }
 
-    fun searchNorth() {
-        var direction = Direction.NORTH
-        var currentIndex = input[animalRowIndex].indexOf('S')
-        var currentRow = animalRowIndex - 1
-        var currentPipe = input[currentRow][currentIndex]
-        var steps = 0
-        while (currentPipe != 'S') {
-            direction = updateDirection(currentPipe, direction)
-            currentRow = updateRow(currentRow, direction)
-            currentIndex = updateIndex(currentIndex, direction)
-            currentPipe = input[currentRow][currentIndex]
-            steps += 1
-        }
-        if ((steps + 1) / 2 > maxSteps)  maxSteps = (steps + 1) / 2
-    }
-
-    fun searchSouth() {
-        var direction = Direction.SOUTH
-        var currentIndex = input[animalRowIndex].indexOf('S')
-        var currentRow = animalRowIndex + 1
-        var currentPipe = input[currentRow][currentIndex]
-        var steps = 0
-        while (currentPipe != 'S') {
-            direction = updateDirection(currentPipe, direction)
-            currentRow = updateRow(currentRow, direction)
-            currentIndex = updateIndex(currentIndex, direction)
-            currentPipe = input[currentRow][currentIndex]
-            steps += 1
-        }
-        if ((steps + 1) / 2 > maxSteps)  maxSteps = (steps + 1) / 2
-    }
-
-    fun updateIndex(currentIndex: Int, direction: Direction): Int {
+    private fun updateIndex(currentIndex: Int, direction: Direction): Int {
         return when(direction) {
             Direction.EAST -> currentIndex + 1
             Direction.WEST -> currentIndex - 1
             else -> currentIndex
         }
     }
-
-
-    fun updateRow(currentRow: Int, direction: Direction): Int {
+    private fun updateRow(currentRow: Int, direction: Direction): Int {
         return when(direction) {
             Direction.SOUTH -> currentRow + 1
             Direction.NORTH -> currentRow - 1
             else -> currentRow
-        }
-    }
-
-    fun updateDirection(pipe: Char, direction: Direction): Direction {
-        return when(pipe) {
-            'L' -> {
-                if (direction == Direction.WEST) Direction.NORTH else Direction.EAST
-            }
-            'J' ->  if (direction == Direction.SOUTH) Direction.WEST else Direction.NORTH
-            '7' -> if (direction == Direction.EAST) Direction.SOUTH else Direction.WEST
-            'F' -> if (direction == Direction.WEST) Direction.SOUTH else Direction.EAST
-            else -> direction
         }
     }
 }
